42a6646a80bbcc89c704f5421edb7427fcd979c8
alexanch <alexboyko94@gmail.com>
2020-01-29 23:18:57 +0100

    Update quickSort.py

--- quickSort.py
+++ quickSort.py
@@ -1,7 +1,8 @@
-def quicksort(array):
-    """Implement quick sort in Python.
-        Input a list.
-        Output a sorted list."""
+def quickSort(array):
+    """
+    Input: a list.
+    Output: a sorted list.
+    """
     
     # check if array is bigger than 1:
     if array==[] or len(array) == 1:
@@ -15,6 +16,7 @@ def quicksort(array):
         # if k-element is bigger: switch elements (k-element to pivot, pivot to pivot-1, pivot-1 to k-element)
         # >>> illustration: https://prnt.sc/qcjqgw
         # if k-element is smaller: increase k-index by 1
+        
         while k<pivot:
             if array[k]>=array[pivot]:
                 temp1,temp2,temp3 = array[pivot],array[pivot-1],array[k]
@@ -22,7 +24,13 @@ def quicksort(array):
                 pivot = pivot-1
             elif array[k]<array[pivot]:
                 k+=1
-                
         # apply recursive calls for unsorted subarrays        
         array_before_pivot,array_after_pivot = array[:pivot], array[pivot+1:]
+        
     return quicksort(array_before_pivot)+[array[pivot]]+quicksort(array_after_pivot)
+
+"""
+Test case:
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print quicksort(test)
+"""
